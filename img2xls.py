a4429d9aff2b2f4edc5bb7b5e39298f48c9ed8b3
Fabian Würfl <fabian.wuerfl@gmx.at>
2015-07-30 09:52:55 +0200

    [FIX] Fixed crash when image succeeds specific size (in pixels)
    The bug was caused due to the limitation of a maximum of 4094 different XFs
    (styles) in the xlwt-module. The feature which introduced the possibility to
    add grids to the Excel sheet created a new style for every pixel.
    
    This behaviour is now changed: The different style-variations (no border, top
    border, left border, and top & left border) are now generated in advance and
    re-used whenever they are needed.
    
    Tested with images of various sizes (up to 1600x900) and different grid
    settings.
    Tested on Linux Mint 17.1 (64-bit) and LibreOffice 4.2.8.2

--- img2xls.py
+++ img2xls.py
@@ -7,6 +7,12 @@ import os
 import re
 from PIL import Image
 
+"""constants for accessing the correct style-variation"""
+STYLE_COUNT = 4
+STYLE_OFFSET_BORDER_TOP = 1
+STYLE_OFFSET_BORDER_LEFT = 2
+STYLE_OFFSET_BORDER_TOPLEFT = 3
+
 def load_image_rgb(path):
     """Ensures the image to be in RGB format."""
     img = Image.open(path)
@@ -71,14 +77,38 @@ def gen_style_lookup(img, pal_img, book):
         palcolnum = pal_pixels[x_pos, y_pos]
         if palcolnum in already_used_colors:
             return
+        """We need to define a style for every possible style-combination"""
+        """In our case: 1) cell without border 2) cell with top border"""
+        """ 3) cell with left border 4) cell with top & left border"""
+
         already_used_colors.add(palcolnum)
         col_name = "custom_colour_" + str(palcolnum)
         xlwt.add_palette_colour(col_name, palcolnum)
         book.set_colour_RGB(palcolnum, *img_pixels[x_pos, y_pos])
+
+        """cell without borders"""
         style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + col_name)
         style.pattern.pattern_fore_colour = palcolnum
-        style_lookup[palcolnum] = style
+        style_lookup[palcolnum * STYLE_COUNT] = style
 
+        """cell with top border"""
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + col_name)
+        style.pattern.pattern_fore_colour = palcolnum
+        style.borders.top = 1
+        style_lookup[palcolnum * STYLE_COUNT + STYLE_OFFSET_BORDER_TOP] = style
+
+        """cell with left border"""
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + col_name)
+        style.pattern.pattern_fore_colour = palcolnum
+        style.borders.left = 1
+        style_lookup[palcolnum * STYLE_COUNT + STYLE_OFFSET_BORDER_LEFT] = style
+
+        """cell with topleft border"""
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + col_name)
+        style.pattern.pattern_fore_colour = palcolnum
+        style.borders.top = 1
+        style.borders.left = 1
+        style_lookup[palcolnum*STYLE_COUNT +STYLE_OFFSET_BORDER_TOPLEFT] = style
 
     map2d(img.size, add_style_lookup)
 
@@ -90,17 +120,27 @@ def set_cell_colors(pal_img, style_lookup, sheet,   grid_gap_vert = 0,
     pal_pixels = pal_img.load()
     def write_sheet_cell(x_pos, y_pos):
         """Set a single pixel, i.e. cell, in table."""
-        """"first retrieve the styles-settings for this pixel (color)"""
-        style = style_lookup[pal_pixels[x_pos, y_pos]]
-        """Add borders to the cell as needed"""
-        border = xlwt.Borders()
-        """testing for a positive value because x modulo a negative number is
-        always zero """
-        if grid_gap_vert > 0 and x_pos % grid_gap_vert == 0:
-            border.left = 1
-        if grid_gap_horiz > 0 and y_pos % grid_gap_horiz == 0:
-            border.top = 1
-        style.borders = border
+
+        palcolnum = pal_pixels[x_pos, y_pos]
+        if grid_gap_vert > 0 or grid_gap_horiz > 0:
+            """Testing for a positive value because x modulo a negative number
+            is always zero """
+            if grid_gap_vert > 0 and x_pos % grid_gap_vert == 0:
+                if grid_gap_horiz > 0 and y_pos % grid_gap_horiz == 0:
+                    idx = palcolnum * STYLE_COUNT + STYLE_OFFSET_BORDER_TOPLEFT
+                    style = style_lookup[idx]
+                else:
+                    idx = palcolnum * STYLE_COUNT + STYLE_OFFSET_BORDER_LEFT
+                    style = style_lookup[idx]
+            elif grid_gap_horiz > 0 and y_pos % grid_gap_horiz == 0:
+                idx = palcolnum * STYLE_COUNT + STYLE_OFFSET_BORDER_TOP
+                style = style_lookup[idx]
+            else:
+                """No borders needed"""
+                style = style_lookup[palcolnum * STYLE_COUNT]
+        else:
+            """No borders wanted"""
+            style = style_lookup[palcolnum * STYLE_COUNT]
         """Write style for the cell to the sheet"""
         sheet.write(y_pos, x_pos, ' ', style)
 
